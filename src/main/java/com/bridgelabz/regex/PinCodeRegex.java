c21dd8141928c753a7864aebaedc87618613e125
NivruttiWagh <nivruttiwagh2002@gmail.com>
2024-09-13 22:05:59 +0530

    Added : Restrict Code Logic

--- src/main/java/com/bridgelabz/regex/PinCodeRegex.java
+++ src/main/java/com/bridgelabz/regex/PinCodeRegex.java
@@ -7,12 +7,15 @@ public class PinCodeRegex {
     public static final String PINCODE_PATTERN = "^[1-9][0-9]{5}$";
     public static boolean validatePinCode(String pinCode)
     {
+        if (!Character.isDigit(pinCode.charAt(0))) {
+            return false;
+        }
         Pattern pattern = Pattern.compile(PINCODE_PATTERN);
         Matcher matcher = pattern.matcher(pinCode);
         return matcher.matches();
     }
     public static void main(String[] args) {
-        String pincodeToValidate = "400088";
+        String pincodeToValidate = "A400088";
 
         if(validatePinCode(pincodeToValidate)){
             System.out.println(pincodeToValidate+ " is valid pincode.");
