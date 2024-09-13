31c90aa00c9a7498bf78502f34e5d281dc19eef0
NivruttiWagh <nivruttiwagh2002@gmail.com>
2024-09-13 22:24:23 +0530

    Added : Space Validator

--- src/main/java/com/bridgelabz/regex/PinCodeRegex.java
+++ src/main/java/com/bridgelabz/regex/PinCodeRegex.java
@@ -4,7 +4,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class PinCodeRegex {
-    public static final String PINCODE_PATTERN = "^[1-9][0-9]{5}$";
+    public static final String PINCODE_PATTERN = "^[1-9][0-9]{2}\\s?[0-9]{3}$";
     public static boolean validatePinCode(String pinCode)
     {
         if (!Character.isDigit(pinCode.charAt(0))) {
@@ -18,12 +18,18 @@ public class PinCodeRegex {
         return matcher.matches();
     }
     public static void main(String[] args) {
-        String pincodeToValidate = "400088B";
+        String pincodeToValidate1 = "400088";
+        String pincodeToValidate2 = "400 088";
 
-        if(validatePinCode(pincodeToValidate)){
-            System.out.println(pincodeToValidate+ " is valid pincode.");
+        if(validatePinCode(pincodeToValidate1)){
+            System.out.println(pincodeToValidate1+ " is valid pincode.");
+        }else{
+            System.out.println("Invalid Pincode..!");
         }
-        else{
+
+        if (validatePinCode(pincodeToValidate2)) {
+            System.out.println(pincodeToValidate2 + " is also a valid pincode.");
+        } else {
             System.out.println("Invalid Pincode..!");
         }
     }
