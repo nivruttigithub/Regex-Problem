549ded97e2257a5c051784fb09b2b3a8130c028b
NivruttiWagh <nivruttiwagh2002@gmail.com>
2024-09-13 22:01:01 +0530

    Added : Pin Validate Program

--- src/main/java/com/bridgelabz/regex/PinCodeRegex.java
+++ src/main/java/com/bridgelabz/regex/PinCodeRegex.java
@@ -1,7 +1,24 @@
 package com.bridgelabz.regex;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class PinCodeRegex {
+    public static final String PINCODE_PATTERN = "^[1-9][0-9]{5}$";
+    public static boolean validatePinCode(String pinCode)
+    {
+        Pattern pattern = Pattern.compile(PINCODE_PATTERN);
+        Matcher matcher = pattern.matcher(pinCode);
+        return matcher.matches();
+    }
     public static void main(String[] args) {
-        System.out.println("Welcome to Regex Program");
+        String pincodeToValidate = "400088";
+
+        if(validatePinCode(pincodeToValidate)){
+            System.out.println(pincodeToValidate+ " is valid pincode.");
+        }
+        else{
+            System.out.println("Invalid Pincode..!");
+        }
     }
 }
