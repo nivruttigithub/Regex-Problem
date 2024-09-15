093bbee1d0828e87cb4780d672b666dcca629244
NivruttiWagh <nivruttiwagh2002@gmail.com>
2024-09-15 16:19:57 +0530

    Refactor : Ensure . After bridgelabz & validate mandatory 3rd part

--- src/main/java/com/bridgelabz/regex/EmailRegex.java
+++ src/main/java/com/bridgelabz/regex/EmailRegex.java
@@ -7,11 +7,14 @@ public class EmailRegex {
 //    Validate Email address with a regex. The email consists of minimum 3 and
 //    optional 2 more parts with mandatory @ and . abc.xyz@bridgelabz.co.in
 //    Here abc, bridgelabz and co are mandatory and the remaining 2 are optional.
+
     // UC1 : lets validate the mandatory part and start with abc
 
     //UC2 : Ensure @ and validate the mandatory 2nd part i.e. bridgelabz
 
-    private static final String EMAIL_PATTERN = "^[a-z]{3}\\.?[a-z]*@[a-z]{10}.*$";
+    //UC3 : Ensure “.” after bridgelabz and validate the mandatory 3rd part i.e. co
+
+    private static final String EMAIL_PATTERN = "^[a-z]{3}\\.?[a-z]*@[a-z]{10}\\.[a-z]{2}.*$";
 
     public static boolean validateEmail(String email) {
         Pattern pattern = Pattern.compile(EMAIL_PATTERN);
